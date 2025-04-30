8f54e1d1b33c7cba827fa52bf19316f273b450dd
Clemerson Cristiano <clemersoncristiano20@gmail.com>
2025-04-30 14:48:04 -0300

    adição de correção de bug no menu ao redimensionar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
 
 let botaoMenu = document.getElementById("botaoMenuMobile");
 let menuMobile = document.getElementById("menuMobile");
+let ativado = false;
 
 botaoMenu.addEventListener("click", () => {
-
     if (menuMobile.style.display === "flex" && window.innerWidth < 668) {
         menuMobile.style.display = "none";
-
     } else {
         menuMobile.style.display = "flex";
+    }
+});
+
+// Adicionando o evento de redimensionamento para ocultar o menu mobile em telas maiores
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 668) {
+        menuMobile.style.display = "none";
     }
 });
